0e825cc44ce711c5206f8247847b2803d3905e63
weter123 <algailani.ahmad@gmail.com>
2023-02-04 17:22:47 -0500

    added icons to bottom bar

--- src/components/BottomTabBar.js
+++ src/components/BottomTabBar.js
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import Schedule from './Schedule';
 import Social from './Social';
 //import Setting from './Setting';
@@ -13,11 +15,46 @@ const BottomTabBar = () => {
             screenOptions={{
                 headerShown: false,
             }}>
-            <Tab.Screen name ='Dashboard' component={UserNav} />
-            <Tab.Screen name='Workout' component={Workout} />
-            <Tab.Screen name='Schedule' component={Schedule} />
-            <Tab.Screen name='Social' component={Social} />
-            <Tab.Screen name='Settings' component={Social} />
+            <Tab.Screen name ='Dashboard' component={UserNav} 
+                options={{
+                    tabBarLabel: 'Dashboard',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="view-dashboard" color={color} size={size} />
+                    ),
+                  }}
+            />
+            <Tab.Screen name='Workout' component={Workout} 
+                 options={{
+                    tabBarLabel: 'Workout',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="dumbbell" color={color} size={size} />
+                    ),
+                  }}
+            />
+             <Tab.Screen name='Social' component={Social} 
+             options={{
+                tabBarLabel: 'Social',
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons name="account-multiple" color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen name='Schedule' component={Schedule} 
+                 options={{
+                    tabBarLabel: 'Schedule',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="calendar-blank" color={color} size={size} />
+                    ),
+                  }}
+            />
+            <Tab.Screen name='Settings' component={Social} 
+                 options={{
+                    tabBarLabel: 'Dashboard',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="cog" color={color} size={size} />
+                    ),
+                  }}
+            />
         </Tab.Navigator>
     );
 };
