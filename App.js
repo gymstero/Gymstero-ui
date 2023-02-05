ffe3c4efbc5d34734f088994cc32c26bf0795002
Wonkeun No <zakk23v@hotmail.com>
2023-02-04 21:24:10 -0500

    Refactor setting components

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AuthOptions from './src/components/auth/AuthOptions'
+import AuthOptions from './src/components/auth/AuthOptions';
 import Signup from './src/components/auth/Signup';
 import Login from './src/components/auth/Login';
-import BottomTabBar from './src/components/BottomTabBar'
+import BottomTabBar from './src/components/BottomTabBar';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Authentication' screenOptions={{ headerShown: true }}>
-        <Stack.Screen name="Authentication" component={AuthOptions} />
-        <Stack.Screen name="Sign-up" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={BottomTabBar} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName='Authentication'
+                screenOptions={{ headerShown: true }}>
+                <Stack.Screen name='Authentication' component={AuthOptions} />
+                <Stack.Screen name='Sign-up' component={Signup} />
+                <Stack.Screen name='Login' component={Login} />
+                <Stack.Screen name='Home' component={BottomTabBar} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
