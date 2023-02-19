4d6649dc19481ea63c48ee9fdbd7c182fbee600e
ArazTey <ateymourzadeh@myseneca.ca>
2023-02-19 01:58:31 -0500

    ImageButton component made under Layout folder, BottomBar colors updated and Dashboard has a new look utilizing ImageBotton and new colors

--- src/components/BottomTabBar.js
+++ src/components/BottomTabBar.js
@@ -1,92 +1,92 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-
+import { theme } from "../theme/theme";
 import Schedule from "./Schedule";
 import Social from "./Social";
 import Setting from "./Setting/Setting";
 import WorkoutNav from "./WorkoutPages/WorkoutNav";
 import UserNav from "./User/UserNav";
+import { NativeBaseProvider, View } from "native-base";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabBar = () => {
   return (
-      <Tab.Navigator
-          screenOptions={{
-              headerShown: true,
-              headerTitleAlign: 'center',
-          }}>
-          <Tab.Screen
-              name='Dashboard'
-              component={UserNav}
-              options={{
-                  tabBarLabel: 'Dashboard',
-                  tabBarIcon: ({ color, size }) => (
-                      <MaterialCommunityIcons
-                          name='view-dashboard'
-                          color={color}
-                          size={size}
-                      />
-                  ),
-              }}
-          />
-          <Tab.Screen
-              name='Workout'
-              component={WorkoutNav}
-              options={{
-                  tabBarLabel: 'Workout',
-                  tabBarIcon: ({ color, size }) => (
-                      <MaterialCommunityIcons
-                          name='dumbbell'
-                          color={color}
-                          size={size}
-                      />
-                  ),
-              }}
-          />
-          <Tab.Screen
-              name='Social'
-              component={Social}
-              options={{
-                  tabBarLabel: 'Social',
-                  tabBarIcon: ({ color, size }) => (
-                      <MaterialCommunityIcons
-                          name='account-multiple'
-                          color={color}
-                          size={size}
-                      />
-                  ),
-              }}
-          />
-          <Tab.Screen
-              name='Schedule'
-              component={Schedule}
-              options={{
-                  tabBarLabel: 'Schedule',
-                  tabBarIcon: ({ color, size }) => (
-                      <MaterialCommunityIcons
-                          name='calendar-blank'
-                          color={color}
-                          size={size}
-                      />
-                  ),
-              }}
-          />
-          <Tab.Screen
-              name='Settings'
-              component={Setting}
-              options={{
-                  tabBarLabel: 'Setting',
-                  tabBarIcon: ({ color, size }) => (
-                      <MaterialCommunityIcons
-                          name='cog'
-                          color={color}
-                          size={size}
-                      />
-                  ),
-              }}
-          />
-      </Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        headerTitleAlign: "center",
+        tabBarActiveTintColor: "#D23812",
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarPosition: "bottom",
+        tabBarStyle: {
+          backgroundColor: "#000000",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={UserNav}
+        options={{
+          tabBarLabel: "Dashboard",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="view-dashboard"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Workout"
+        component={WorkoutNav}
+        options={{
+          tabBarLabel: "Workout",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="dumbbell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Social"
+        component={Social}
+        options={{
+          tabBarLabel: "Social",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-multiple"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Schedule"
+        component={Schedule}
+        options={{
+          tabBarLabel: "Schedule",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="calendar-blank"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Setting}
+        options={{
+          tabBarLabel: "Setting",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
---
+++ src/components/Layout/ImageButton.js
@@ -0,0 +1,65 @@
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
+import { Text } from "native-base";
+
+const ImageButton = ({
+  imageSource,
+  text,
+  buttonStyle,
+  imageStyle,
+  textStyle,
+  onPress,
+}) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      style={[styles.button, buttonStyle]}
+      onPress={onPress}
+    >
+      <ImageBackground
+        style={[styles.imageContainer]}
+        imageStyle={[styles.image, imageStyle]}
+        source={imageSource}
+      >
+        <Text style={[styles.text, textStyle]}>{text}</Text>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  button: {
+    flex: 1,
+    width: "100%",
+    marginTop: 20,
+    borderRadius: 10,
+    overflow: "hidden",
+    alignItems: "center",
+    textAlign: "center",
+    height: 120,
+  },
+  imageContainer: {
+    backgroundColor: "black",
+    width: "100%",
+    alignItems: "center",
+    textAlign: "center",
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    opacity: 0.6,
+  },
+  text: {
+    color: "#FFF0EE",
+    fontWeight: "bold",
+    top: "35%",
+    marginTop: 10,
+    position: "absolute",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    fontSize: 20,
+    fontFamily: "Arial",
+  },
+};
+
+export default ImageButton;
