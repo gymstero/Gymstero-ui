11eeecb7f4cbc38fa7dee243689b51a80b28a09e
ArazTey <ateymourzadeh@myseneca.ca>
2023-02-19 02:14:25 -0500

    UpdatePanel and UserInfo Panel new button colors

--- src/components/Setting/UserInfoPanel.js
+++ src/components/Setting/UserInfoPanel.js
@@ -1,93 +1,94 @@
-import { Box, Button, Container, Flex, Image, Text, Spacer } from 'native-base';
-import { googleLogout } from '../auth/auth';
-import { useNavigation } from '@react-navigation/native';
+import { Box, Button, Container, Flex, Image, Text, Spacer } from "native-base";
+import { googleLogout } from "../auth/auth";
+import { useNavigation } from "@react-navigation/native";
+import { theme } from "../../theme/theme";
 
 const UserInfoPanel = ({
-    userData,
-    setEditMode,
-    error,
-    setError,
-    setUserMessage,
+  userData,
+  setEditMode,
+  error,
+  setError,
+  setUserMessage,
 }) => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const updateButtonPressed = () => {
-        setEditMode(true);
-    };
+  const updateButtonPressed = () => {
+    setEditMode(true);
+  };
 
-    return (
-        <>
-            <Container mt={5}>
-                <Flex flexDirection='row'>
-                    <Image
-                        source={{
-                            uri:
-                                userData.photoURL ||
-                                'https://img.icons8.com/ios-glyphs/90/000000/user--v1.png',
-                        }}
-                        alt='user profile'
-                        size='xl'
-                        flex={2}
-                    />
-                    <Container mx={5} flex={3} my='auto'>
-                        <Text fontSize={16} fontWeight={600} mb={1}>
-                            Username
-                        </Text>
-                        <Text>{userData.username}</Text>
-                    </Container>
-                </Flex>
-                <Container my={5}>
-                    <Text fontSize={16} fontWeight={600} mb={1}>
-                        User Description
-                    </Text>
-                    <Box width='100%' height={100} shadow={2}>
-                        <Text>{userData.description}</Text>
-                    </Box>
-                </Container>
-                <Container my={5}>
-                    <Flex flexDirection='row' alignItems='center'>
-                        <Text fontSize={16} fontWeight={600}>
-                            Public User
-                        </Text>
-                        <Spacer />
-                        <Text>{userData.publicUser ? 'Yes' : 'No'}</Text>
-                    </Flex>
-                </Container>
-                <Container mt={10} mx='auto'>
-                    <Box w='85%'>
-                        <Button
-                            rounded='full'
-                            width={230}
-                            p='2'
-                            mb={5}
-                            onPress={updateButtonPressed}>
-                            Update
-                        </Button>
-                        <Button
-                            rounded='full'
-                            width={230}
-                            p='2'
-                            onPress={() =>
-                                googleLogout(
-                                    setError,
-                                    setUserMessage,
-                                    navigation
-                                )
-                            }>
-                            Log Out
-                        </Button>
-                        {error ? (
-                            <Text mt={2} fontSize='sm' color='red.600'>
-                                {userMessage}
-                            </Text>
-                        ) : (
-                            <></>
-                        )}
-                    </Box>
-                </Container>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container mt={5}>
+        <Flex flexDirection="row">
+          <Image
+            source={{
+              uri:
+                (userData && userData.photoURL) ||
+                "https://img.icons8.com/ios-glyphs/90/000000/user--v1.png",
+            }}
+            alt="user profile"
+            size="xl"
+            flex={2}
+          />
+          <Container mx={5} flex={3} my="auto">
+            <Text fontSize={16} fontWeight={600} mb={1}>
+              Username
+            </Text>
+            <Text>{userData && userData.username}</Text>
+          </Container>
+        </Flex>
+        <Container my={5}>
+          <Text fontSize={16} fontWeight={600} mb={1}>
+            User Description
+          </Text>
+          <Box width="100%" height={100} shadow={2}>
+            <Text>{userData && userData.description}</Text>
+          </Box>
+        </Container>
+        <Container my={5}>
+          <Flex flexDirection="row" alignItems="center">
+            <Text fontSize={16} fontWeight={600}>
+              Public User
+            </Text>
+            <Spacer />
+            <Text>{userData && userData.publicUser ? "Yes" : "No"}</Text>
+          </Flex>
+        </Container>
+        <Container mt={10} mx="auto">
+          <Box w="85%">
+            <Button
+              backgroundColor={theme.colors.primary}
+              color={theme.colors.text}
+              rounded="full"
+              width={230}
+              p="2"
+              mb={5}
+              onPress={updateButtonPressed}
+            >
+              Update
+            </Button>
+            <Button
+              backgroundColor={theme.colors.secondary}
+              color={theme.colors.text}
+              rounded="full"
+              width={230}
+              p="2"
+              onPress={() => googleLogout(setError, setUserMessage, navigation)}
+            >
+              Log Out
+            </Button>
+            {error ? (
+              <Text mt={2} fontSize="sm" color="red.600">
+                {userMessage}
+              </Text>
+            ) : (
+              <></>
+            )}
+          </Box>
+        </Container>
+      </Container>
+    </>
+  );
 };
 
 export default UserInfoPanel;
