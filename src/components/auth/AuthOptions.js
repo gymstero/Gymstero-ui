b4f68fbe8759b934bc5485a61fee0e2bd0edb98d
ArazTey <ateymourzadeh@myseneca.ca>
2023-03-05 23:01:46 -0500

    commit message

--- src/components/auth/AuthOptions.js
+++ src/components/auth/AuthOptions.js
@@ -6,36 +6,51 @@ import {
   VStack,
   NativeBaseProvider,
   Divider,
-  ScrollView,
+  Image,
+  View,
 } from "native-base";
+import Logo from "../../images/gymsteroLogo.png";
 const AuthOptions = () => {
   const navigation = useNavigation();
 
   return (
     <NativeBaseProvider flex={1} bg="red">
-      <ScrollView h="80%">
-        <VStack alignItems="center" justifyContent="center" mt="350" space={4}>
-          <Button
-            backgroundColor={theme.colors.secondary}
-            rounded="full"
-            w="85%"
-            onPress={() => navigation.navigate("Login")}
-          >
-            Login
-          </Button>
-          <Divider w="80%" />
-          <Button
-            backgroundColor={theme.colors.primary}
-            textDecorationColor={theme.colors.text}
-            rounded="full"
-            w="85%"
-            variant="outline"
-            onPress={() => navigation.navigate("Sign-up")}
-          >
-            <Text color={theme.colors.text}>Sign-up</Text>
-          </Button>
-        </VStack>
-      </ScrollView>
+      <VStack alignItems="center" justifyContent="center" space={4}>
+        <View
+          style={{
+            maxWidth: "100%",
+            maxHeight: 400,
+          }}
+        >
+          <Image
+            style={{
+              flex: 1,
+              resizeMode: "cover",
+            }}
+            alt="Logo"
+            source={Logo}
+          />
+        </View>
+        <Button
+          backgroundColor={theme.colors.secondary}
+          rounded="full"
+          w="85%"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
+        </Button>
+        <Divider w="80%" />
+        <Button
+          backgroundColor={theme.colors.primary}
+          textDecorationColor={theme.colors.text}
+          rounded="full"
+          w="85%"
+          variant="outline"
+          onPress={() => navigation.navigate("Sign-up")}
+        >
+          <Text color={theme.colors.text}>Sign-up</Text>
+        </Button>
+      </VStack>
     </NativeBaseProvider>
   );
 };
