e087d8dfe299217b11599912592526201bc4c48c
Araz Teymourzadeh <78897920+ArazTey@users.noreply.github.com>
2023-03-09 23:29:27 -0500

    Update ExerciseNav.js

--- src/components/Exercise/ExerciseNav.js
+++ src/components/Exercise/ExerciseNav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ExerciseDetails from "./ExerciseDetails";
-import { theme } from "../../theme/globalTheme";
+import { theme } from "../../theme/theme";
 
 const Work = createNativeStackNavigator();
 
