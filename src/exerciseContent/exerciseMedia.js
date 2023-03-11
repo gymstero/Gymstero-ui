fa7aa7a3f957f2730eea06d16c6fe14869d95e6f
ArazTey <ateymourzadeh@myseneca.ca>
2023-03-11 17:27:04 -0500

    Training videos changed to exercise videos , Exercise media object created, exercise content will contain exercise pictures

---
+++ src/exerciseContent/exerciseMedia.js
@@ -0,0 +1,154 @@
+const exerciseMedia = {
+  "1XJ0RGhA4C8QqDJavqMO": {
+    video: require("./exerciseVideos/1XJ0RGhA4C8QqDJavqMO.mp4"),
+  },
+  "3jK1NJRWwxFZqrCvTneh": {
+    video: require("./exerciseVideos/3jK1NJRWwxFZqrCvTneh.mp4"),
+  },
+  "4KqawbAdv6S5MFLTXyg9": {
+    video: require("./exerciseVideos/4KqawbAdv6S5MFLTXyg9.mp4"),
+  },
+  "5ITyFr652MT6O3zojxoO": {
+    video: require("./exerciseVideos/5ITyFr652MT6O3zojxoO.mp4"),
+  },
+  "63yBPYfANPKUNeHxW2wl": {
+    video: require("./exerciseVideos/63yBPYfANPKUNeHxW2wl.mp4"),
+  },
+  "9hn6zh5wncHfo05ontbq": {
+    video: require("./exerciseVideos/9hn6zh5wncHfo05ontbq.mp4"),
+  },
+  "9vE6wEme0sJ4StuzSy9Q": {
+    video: require("./exerciseVideos/9vE6wEme0sJ4StuzSy9Q.mp4"),
+  },
+  A31gG4qQGQIbH4esLvIY: {
+    video: require("./exerciseVideos/A31gG4qQGQIbH4esLvIY.mp4"),
+  },
+  ADDDWXyPc3T1J5H6lk9V: {
+    video: require("./exerciseVideos/ADDDWXyPc3T1J5H6lk9V.mp4"),
+  },
+  AdIoGtUyDnySdgqolMoM: {
+    video: require("./exerciseVideos/AdIoGtUyDnySdgqolMoM.mp4"),
+  },
+  AOlbu29XrVIBPJpOTLSU: {
+    video: 'require("./exerciseVideos/AOlbu29XrVIBPJpOTLSU.mp4")',
+  },
+  AzH7gRuZqdHPYhDFHF4v: {
+    video: require("./exerciseVideos/AzH7gRuZqdHPYhDFHF4v.mp4"),
+  },
+  boV2IKNHFhwiY9A6npIA: {
+    video: require("./exerciseVideos/boV2IKNHFhwiY9A6npIA.mp4"),
+  },
+  ChrqiH6UzkLKt6J9e4Rs: {
+    video: require("./exerciseVideos/ChrqiH6UzkLKt6J9e4Rs.mp4"),
+  },
+  CSV0bLJneI3j586u7BcS: {
+    video: require("./exerciseVideos/CSV0bLJneI3j586u7BcS.mp4"),
+  },
+  cx2b15OKYOAmsB7wpgis: {
+    video: require("./exerciseVideos/cx2b15OKYOAmsB7wpgis.mp4"),
+  },
+  DKNZso4okyMFR4HPSBVj: {
+    video: require("./exerciseVideos/DKNZso4okyMFR4HPSBVj.mp4"),
+  },
+  ETDTY47ZPVfhc0KPmXiW: {
+    video: require("./exerciseVideos/ETDTY47ZPVfhc0KPmXiW.mp4"),
+  },
+  eXPmyJ50lJEKWKilFTug: {
+    video: require("./exerciseVideos/eXPmyJ50lJEKWKilFTug.mp4"),
+  },
+  fCfTVKDmIkPoq7bIeOrn: {
+    video: require("./exerciseVideos/fCfTVKDmIkPoq7bIeOrn.mp4"),
+  },
+  FLsGvhmg5duBoWaKcyZw: {
+    video: require("./exerciseVideos/FLsGvhmg5duBoWaKcyZw.mp4"),
+  },
+  FXHkf3NczAZwHbopnedo: {
+    video: require("./exerciseVideos/FXHkf3NczAZwHbopnedo.mp4"),
+  },
+  gUy6M2hE0EoQnsoVX2ky: {
+    video: require("./exerciseVideos/gUy6M2hE0EoQnsoVX2ky.mp4"),
+  },
+  gxVp4Jg2PvKuiKHfWJpe: {
+    video: require("./exerciseVideos/gxVp4Jg2PvKuiKHfWJpe.mp4"),
+  },
+  IEArUW3kROg0Iq2pQ4fY: {
+    video: require("./exerciseVideos/IEArUW3kROg0Iq2pQ4fY.mp4"),
+  },
+  IsxQDbKXDg8zhbz9gj6v: {
+    videos: require("./exerciseVideos/IsxQDbKXDg8zhbz9gj6v.mp4"),
+  },
+  kRPMKvbt4rqcAoM0v0dM: {
+    video: require("./exerciseVideos/kRPMKvbt4rqcAoM0v0dM.mp4"),
+  },
+  kS97k8QmSRcfHbXyWRQM: {
+    video: require("./exerciseVideos/kS97k8QmSRcfHbXyWRQM.mp4"),
+  },
+  la9ee8Bo6H1hQwjHimmE: {
+    video: require("./exerciseVideos/la9ee8Bo6H1hQwjHimmE.mp4"),
+  },
+  NeDrRhy0MW9Dh2V4MVHk: {
+    video: require("./exerciseVideos/NeDrRhy0MW9Dh2V4MVHk.mp4"),
+  },
+  nzbF9FgWLbKag3h8GCcq: {
+    video: require("./exerciseVideos/nzbF9FgWLbKag3h8GCcq.mp4"),
+  },
+  O4t1WsBVxZ7oGGQldAGL: {
+    video: require("./exerciseVideos/O4t1WsBVxZ7oGGQldAGL.mp4"),
+  },
+  ptixVI5fnk43gT9Y2GJO: {
+    video: require("./exerciseVideos/ptixVI5fnk43gT9Y2GJO.mp4"),
+  },
+  qhf8M0VmOB7lSbSduTwL: {
+    video: require("./exerciseVideos/qhf8M0VmOB7lSbSduTwL.mp4"),
+  },
+  qMXKQ7PIUVJU4Ycl9Co3: {
+    video: require("./exerciseVideos/qMXKQ7PIUVJU4Ycl9Co3.mp4"),
+  },
+  R3fATVav4qmplmmDJonk: {
+    video: require("./exerciseVideos/R3fATVav4qmplmmDJonk.mp4"),
+  },
+  RjS5gosbf5yoGR6ZD2PG: {
+    video: require("./exerciseVideos/RjS5gosbf5yoGR6ZD2PG.mp4"),
+  },
+  RVHDQ2F3y89IugRm7gAu: {
+    video: require("./exerciseVideos/RVHDQ2F3y89IugRm7gAu.mp4"),
+  },
+  sc8Kjs54i7qRb1W4hqut: {
+    video: require("./exerciseVideos/sc8Kjs54i7qRb1W4hqut.mp4"),
+  },
+  sdtbXDbwGy6bqGFqCu9e: {
+    video: require("./exerciseVideos/sdtbXDbwGy6bqGFqCu9e.mp4"),
+  },
+  sKOkQx3tumpkR7Hxz4RI: {
+    video: require("./exerciseVideos/sKOkQx3tumpkR7Hxz4RI.mp4"),
+  },
+  vbVMic7ATyq2iBancdM5: {
+    video: require("./exerciseVideos/vbVMic7ATyq2iBancdM5.mp4"),
+  },
+  vPp8hFFxDL5EFUKyRDpL: {
+    video: require("./exerciseVideos/vPp8hFFxDL5EFUKyRDpL.mp4"),
+  },
+  vpXIQMfsEijixEs3XmJW: {
+    video: require("./exerciseVideos/vpXIQMfsEijixEs3XmJW.mp4"),
+  },
+  WCKWUIOiwGNDBaFCj5fc: {
+    video: require("./exerciseVideos/WCKWUIOiwGNDBaFCj5fc.mp4"),
+  },
+  wW6h9wpusPGL5HIg7nZC: {
+    video: require("./exerciseVideos/wW6h9wpusPGL5HIg7nZC.mp4"),
+  },
+  xFHkEUMXOLIV5Adbn7In: {
+    video: require("./exerciseVideos/xFHkEUMXOLIV5Adbn7In.mp4"),
+  },
+  YDVYGDHnrZwWDjafBwVS: {
+    video: require("./exerciseVideos/YDVYGDHnrZwWDjafBwVS.mp4"),
+  },
+  YMhNqFaTx7KJakEGe0wP: {
+    video: require("./exerciseVideos/YMhNqFaTx7KJakEGe0wP.mp4"),
+  },
+  z3VmcvWKwPk96Lu7SvsE: {
+    video: require("./exerciseVideos/z3VmcvWKwPk96Lu7SvsE.mp4"),
+  },
+};
+
+export default exerciseMedia;
