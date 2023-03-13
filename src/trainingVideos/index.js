29456dc13786f28c7ac9013416e4d926fcdf830f
Wonkeun No <59734889+Genne23v@users.noreply.github.com>
2023-03-12 21:51:17 -0400

    demo preparation (#92)
    * demo preparation
    * Fix schedule submit form
    * Fix colour selection index issue
    * Fix dashboard refresh issue when focused

--- src/trainingVideos/index.js
+++ src/trainingVideos/index.js
@@ -1,5 +1,5 @@
 const VIDEOS = {
-    '9vnKR3QgneTKWChp4X1M': require('./1XJ0RGhA4C8QqDJavqMO.mp4'),
-}
+    '9vnKR3QgneTKWChp4X1M': require('../exerciseContent/exerciseVideos/1XJ0RGhA4C8QqDJavqMO.mp4'),
+};
 
 export default VIDEOS;
