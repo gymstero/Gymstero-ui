0d446d72dfa5d414f7aac9e6e3120e4813d232a5
Wonkeun No <zakk23v@hotmail.com>
2023-03-12 20:46:39 -0400

    demo preparation

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,181 +1,188 @@
 import isEmpty from "lodash/isEmpty";
 import React, { useState, useEffect, useRef, useCallback } from "react";
-import { NativeBaseProvider, Button, VStack, View } from "native-base";
-import AgendaItem from "./AgendaItem";
+import { NativeBaseProvider, Button, VStack, View, Text } from 'native-base';
+import AgendaItem from './AgendaItem';
 import {
-  ExpandableCalendar,
-  AgendaList,
-  CalendarProvider,
-} from "react-native-calendars";
-import { ChevronLeftIcon, ChevronRightIcon } from "native-base";
-import { useNavigation, useIsFocused } from "@react-navigation/native";
-import { getUser, getIdToken } from "../auth/auth";
-import { theme } from "../../theme/theme";
+    ExpandableCalendar,
+    AgendaList,
+    CalendarProvider,
+} from 'react-native-calendars';
+import { ChevronLeftIcon, ChevronRightIcon } from 'native-base';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { getUser, getIdToken } from '../auth/auth';
+import { theme } from '../../theme/theme';
 
-const themeColor = "#00AAAF";
+const themeColor = '#00AAAF';
 const barColourSet = [
-  "#b30000",
-  "#7c1158",
-  "#4421af",
-  "#1a53ff",
-  "#0d88e6",
-  "#00b7c7",
-  "#5ad45a",
-  "#8be04e",
-  "#ebdc78",
+    '#b30000',
+    '#7c1158',
+    '#4421af',
+    '#1a53ff',
+    '#0d88e6',
+    '#00b7c7',
+    '#5ad45a',
+    '#8be04e',
+    '#ebdc78',
 ];
 
 const Schedule = () => {
-  const [workoutScheduleItems, setWorkoutScheduleItems] = useState([]);
+    const [workoutScheduleItems, setWorkoutScheduleItems] = useState([]);
 
-  const isFocused = useIsFocused();
-  const navigation = useNavigation();
-  const marked = useRef(getMarkedDates());
-  const todayBtnTheme = useRef({
-    todayButtonTextColor: themeColor,
-  });
-
-  function getMarkedDates() {
-    const marked = {};
-    workoutScheduleItems.forEach((item) => {
-      if (item.data && item.data.length > 0 && !isEmpty(item.data[0])) {
-        marked[item.title] = { marked: true };
-      } else {
-        marked[item.title] = { disabled: true };
-      }
+    const isFocused = useIsFocused();
+    const navigation = useNavigation();
+    const marked = useRef(getMarkedDates());
+    const todayBtnTheme = useRef({
+        todayButtonTextColor: themeColor,
     });
-    return marked;
-  }
 
-  const addToCalendar = (workouts) => {
-    let tempArray = [];
-
-    if (workouts) {
-      workouts.forEach((workout) => {
-        const randomIndex = Math.floor(Math.random() * 10);
-        if (workout.schedule) {
-          workout.schedule.forEach((date) => {
-            if (
-              tempArray.some(
-                (item) => item.title.split("T")[0] === date.split("T")[0]
-              )
-            ) {
-              let dateFound = tempArray.find(
-                (item) => item.title.split("T")[0] === date.split("T")[0]
-              );
-              dateFound.data.push({
-                id: workout.id,
-                title: workout.title,
-                duration: `${workout.totalWorkoutTime}m`,
-                color: barColourSet[randomIndex],
-              });
+    function getMarkedDates() {
+        const marked = {};
+        workoutScheduleItems.forEach((item) => {
+            if (item.data && item.data.length > 0 && !isEmpty(item.data[0])) {
+                marked[item.title] = { marked: true };
             } else {
-              tempArray.push({
-                title: date,
-                data: [
-                  {
-                    id: workout.id,
-                    title: workout.title,
-                    duration: `${workout.totalWorkoutTime}m`,
-                    color: barColourSet[randomIndex],
-                  },
-                ],
-              });
+                marked[item.title] = { disabled: true };
             }
-          });
+        });
+        return marked;
+    }
+
+    const addToCalendar = (workouts) => {
+        let tempArray = [];
+
+        if (workouts) {
+            workouts.forEach((workout) => {
+                const randomIndex = Math.floor(Math.random() * 10);
+                if (workout.schedule) {
+                    workout.schedule.forEach((date) => {
+                        if (
+                            tempArray.some(
+                                (item) =>
+                                    item.title.split('T')[0] ===
+                                    date.split('T')[0]
+                            )
+                        ) {
+                            let dateFound = tempArray.find(
+                                (item) =>
+                                    item.title.split('T')[0] ===
+                                    date.split('T')[0]
+                            );
+                            dateFound.data.push({
+                                id: workout.id,
+                                title: workout.title,
+                                duration: `${workout.totalWorkoutTime}m`,
+                                color: barColourSet[randomIndex],
+                            });
+                        } else {
+                            tempArray.push({
+                                title: date,
+                                data: [
+                                    {
+                                        id: workout.id,
+                                        title: workout.title,
+                                        duration: `${workout.totalWorkoutTime}m`,
+                                        color: barColourSet[randomIndex],
+                                    },
+                                ],
+                            });
+                        }
+                    });
+                }
+            });
         }
-      });
-    }
 
-    tempArray.sort((a, b) => a.title > b.title);
-    setWorkoutScheduleItems(tempArray);
-  };
-  const fetchWorkoutSchedule = async () => {
-    const userInfo = await getUser();
-    const idToken = await getIdToken();
+        tempArray.sort((a, b) => a.title > b.title);
+        setWorkoutScheduleItems(tempArray);
+    };
+    const fetchWorkoutSchedule = async () => {
+        const userInfo = await getUser();
+        const idToken = await getIdToken();
 
-    fetch(`http://10.0.2.2:8080/api/user/${userInfo.uid}/workout-schedule`, {
-      method: "Get",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${idToken}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.info("Workout fetched", res.workoutSchedule);
-        addToCalendar(res.workoutSchedule);
-      })
-      .catch((err) => {
-        console.warn(err);
-      });
-  };
+        fetch(
+            `http://10.0.2.2:8080/api/user/${userInfo.uid}/workout-schedule`,
+            {
+                method: 'Get',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${idToken}`,
+                },
+            }
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                console.info('Workout fetched', res.workoutSchedule);
+                addToCalendar(res.workoutSchedule);
+            })
+            .catch((err) => {
+                console.warn(err);
+            });
+    };
 
-  useEffect(() => {
-    if (isFocused) {
-      fetchWorkoutSchedule();
-    }
-  }, [isFocused]);
+    useEffect(() => {
+        if (isFocused) {
+            fetchWorkoutSchedule();
+        }
+    }, [isFocused]);
 
-  const onDateChanged = useCallback((date, updateSource) => {
-    console.log("ExpandableCalendarScreen onDateChanged: ", date, updateSource);
-  }, []);
+    const onDateChanged = useCallback((date, updateSource) => {
+        console.log(
+            'ExpandableCalendarScreen onDateChanged: ',
+            date,
+            updateSource
+        );
+    }, []);
 
-  const onMonthChange = useCallback(({ dateString }) => {
-    console.log("ExpandableCalendarScreen onMonthChange: ", dateString);
-  }, []);
+    const onMonthChange = useCallback(({ dateString }) => {
+        console.log('ExpandableCalendarScreen onMonthChange: ', dateString);
+    }, []);
 
-  const renderItem = useCallback(({ item }) => {
-    return <AgendaItem item={item} />;
-  }, []);
+    const renderItem = useCallback(({ item }) => {
+        return <AgendaItem item={item} />;
+    }, []);
 
-  return (
-    <NativeBaseProvider>
-      <View backgroundColor={"white"}>
-        <VStack alignItems="center" justifyContent="center">
-          <Button
-            w="85%"
-            rounded={"full"}
-            backgroundColor={theme.colors.secondary}
-            onPress={() => navigation.navigate("ScheduleWorkout")}
-          >
-            Schedule a Workout
-          </Button>
-        </VStack>
-        <CalendarProvider
-          date={new Date().toISOString()}
-          onDateChanged={onDateChanged}
-          onMonthChange={onMonthChange}
-          showTodayButton
-          disabledOpacity={0.6}
-          theme={todayBtnTheme.current}
-          todayBottomMargin={16}
-        >
-          <ExpandableCalendar
-            disablePan
-            initialPosition={ExpandableCalendar.positions.CLOSED}
-            disableAllTouchEventsForDisabledDays
-            firstDay={1}
-            markedDates={marked.current}
-            leftArrowImageSource={ChevronLeftIcon}
-            rightArrowImageSource={ChevronRightIcon}
-            animateScroll
-            closeOnDayPress={false}
-          />
-          {workoutScheduleItems && workoutScheduleItems.length > 0 ? (
-            <AgendaList
-              sections={workoutScheduleItems}
-              renderItem={renderItem}
-              scrollToNextEvent
-            />
-          ) : (
-            <></>
-          )}
-        </CalendarProvider>
-      </View>
-    </NativeBaseProvider>
-  );
+    return (
+        <NativeBaseProvider>
+            <VStack alignItems='center' justifyContent='center'>
+                <Button
+                    w='85%'
+                    rounded={'full'}
+                    backgroundColor={theme.colors.secondary}
+                    onPress={() => navigation.navigate('ScheduleWorkout')}>
+                    Schedule a Workout
+                </Button>
+            </VStack>
+            <CalendarProvider
+                date={new Date().toISOString()}
+                onDateChanged={onDateChanged}
+                onMonthChange={onMonthChange}
+                showTodayButton
+                disabledOpacity={0.6}
+                theme={todayBtnTheme.current}
+                todayBottomMargin={16}>
+                <ExpandableCalendar
+                    disablePan
+                    initialPosition={ExpandableCalendar.positions.CLOSED}
+                    disableAllTouchEventsForDisabledDays
+                    firstDay={1}
+                    markedDates={marked.current}
+                    leftArrowImageSource={ChevronLeftIcon}
+                    rightArrowImageSource={ChevronRightIcon}
+                    animateScroll
+                    closeOnDayPress={false}
+                />
+                {workoutScheduleItems && workoutScheduleItems.length > 0 ? (
+                    <AgendaList
+                        sections={workoutScheduleItems}
+                        renderItem={renderItem}
+                        scrollToNextEvent
+                    />
+                ) : (
+                    <Text>No Workout Scheduled</Text>
+                )}
+            </CalendarProvider>
+        </NativeBaseProvider>
+    );
 };
 
 export default Schedule;
--- src/components/WorkoutPages/RunWorkout/RunExercise.js
+++ src/components/WorkoutPages/RunWorkout/RunExercise.js
@@ -1,11 +1,19 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { NativeBaseProvider, ScrollView, VStack,  Button, View } from "native-base"
-import { Text} from 'react-native';
-import React, { useEffect, useState} from "react";
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
-import Video from "react-native-video";
-import VIDEOS from "../../../trainingVideos";
-const RunExercise =() => {
+import {
+    NativeBaseProvider,
+    ScrollView,
+    VStack,
+    Button,
+    View,
+    Heading,
+    Box,
+} from 'native-base';
+import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import Video from 'react-native-video';
+import VIDEOS from '../../../trainingVideos';
+const RunExercise = () => {
     const route = useRoute();
     const navigation = useNavigation();
     const [exercises, setExercises] = useState([]);
@@ -15,102 +23,140 @@ const RunExercise =() => {
     const [key, setKey] = useState(0);
     const [reveal, setReveal] = useState(false);
     const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-    const [videoSource, setVideoSource]=useState("");
+    const [videoSource, setVideoSource] = useState('');
 
     const onLayout = (event) => {
-      const { width } = event.nativeEvent.layout;
-      const height = (width * 9) / 10;
-      setDimensions({ width, height });
+        const { width } = event.nativeEvent.layout;
+        const height = (width * 9) / 10;
+        setDimensions({ width, height });
     };
     const { width, height } = dimensions;
 
     const goToNextExercise = () => {
-        if (count < exercises.length -1) {
+        if (count < exercises.length - 1) {
             let num = count + 1;
             setCount(num);
             setIsPlaying(false);
             setReveal(false);
-            setKey(prevKey => prevKey + 1)
-            }
-    }
+            setKey((prevKey) => prevKey + 1);
+        }
+    };
+
     const revealTimer = () => {
-        setTimer(exercises[count].estimatedTime*60);
+        setTimer(exercises[count].estimatedTime * 60);
         setReveal(true);
 
         setIsPlaying(true);
-    }
+    };
+
     const toggleTimer = () => {
-        setTimer(exercises[count].estimatedTime*60);
-        setIsPlaying(prev => !prev);
-    }
-    useEffect(()=> {
-        setExercises(route.params.exercises)
+        setTimer(exercises[count].estimatedTime * 60);
+        setIsPlaying((prev) => !prev);
+    };
+
+    const convertTime = (remainingTime) => {
+        const mins = parseInt(remainingTime / 60);
+        let secs = remainingTime % 60;
+        return secs < 10 ? `${mins}:0${secs}` : `${mins}:${secs}`;
+    };
+
+    useEffect(() => {
+        setExercises(route.params.exercises);
         console.log(route.params.exercises);
-    }, [route])
+    }, [route]);
     return (
         <NativeBaseProvider>
             <ScrollView>
-                { exercises && exercises.length > 0 ? ( 
+                {exercises && exercises.length > 0 ? (
                     <VStack>
-                        <Text>Name: {exercises[count].id}</Text>
-                        <Text>Name: {exercises[count].exerciseInfo.title}</Text>
-                        <Text>Target Sets: {exercises[count].targetSets}</Text>
-                        <Text>Estimated Time: {exercises[count].estimatedTime}</Text>
+                        <Heading mx='auto' mt={4}>
+                            {exercises[count].exerciseInfo.title}
+                        </Heading>
+                        <Box mx={8} mt={4}>
+                            <Text mt={2}>
+                                Target Sets:{' '}
+                                {exercises[count].targetSets
+                                    ? exercises[count].targetSets
+                                    : 'Not Set'}
+                            </Text>
+                            <Text>
+                                Estimated Time:{' '}
+                                {exercises[count].estimatedTime
+                                    ? `${exercises[count].estimatedTime} mins`
+                                    : 'Not Set'}
+                            </Text>
+                        </Box>
                     </VStack>
-                )  : (
-                     <></>
-                ) }
-                
-                {count == exercises.length -1 ? (
-                    <Button onPress={()=> navigation.navigate('WorkoutMainPage')}> Done</Button>
                 ) : (
-                    <Button onPress={goToNextExercise}> Next</Button>
+                    <></>
                 )}
-                <VStack height= '60%' >
+                {count == exercises.length - 1 ? (
+                    <Button
+                        w='85%'
+                        mx='auto'
+                        mt={4}
+                        onPress={() => navigation.navigate('WorkoutMainPage')}>
+                        {' '}
+                        Done
+                    </Button>
+                ) : (
+                    <Button w='85%' mx='auto' mt={4} onPress={goToNextExercise}>
+                        Next Exercise
+                    </Button>
+                )}
+                <VStack w='85%' mx='auto' mt={20}>
                     {reveal ? (
-                        <View style={{ flex: 1 }} onLayout={onLayout}>
+                        <View
+                            style={{ flex: 1, alignSelf: 'center' }}
+                            onLayout={onLayout}>
                             <CountdownCircleTimer
                                 key={key}
                                 isPlaying={isPlaying}
                                 duration={timer}
-                                colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+                                colors={[
+                                    '#004777',
+                                    '#F7B801',
+                                    '#A30000',
+                                    '#A30000',
+                                ]}
                                 colorsTime={[10, 6, 3, 0]}
-                                onComplete={() => ({ shouldRepeat: true, delay: 2 })}
-                                updateInterval={0}
-                            >
-                            {({ remainingTime, color }) => (
-                                <Text style={{ color, fontSize: 40 }}>
-                                    {remainingTime}
-                                </Text>
-                            )}
+                                onComplete={() => ({
+                                    shouldRepeat: true,
+                                    delay: 2,
+                                })}
+                                updateInterval={0}>
+                                {({ remainingTime, color }) => (
+                                    <Text style={{ color, fontSize: 40 }}>
+                                        {convertTime(remainingTime)}
+                                    </Text>
+                                )}
                             </CountdownCircleTimer>
                             {isPlaying ? (
-                                <Button onPress={toggleTimer}>Pause</Button>
-                            )  : (
-                                    <Button onPress={toggleTimer}>Play</Button>
+                                <Button mt={4} onPress={toggleTimer}>
+                                    Pause
+                                </Button>
+                            ) : (
+                                <Button mt={4} onPress={toggleTimer}>
+                                    Resume
+                                </Button>
                             )}
                         </View>
                     ) : (
                         <View style={{ flex: 1 }} onLayout={onLayout}>
-                        <Video
-                            source={VIDEOS['9vnKR3QgneTKWChp4X1M']} // just update the source for example source={{ uri: 'http://www.example.com/video.mp4' }}
-                            style={{ width, height }}
-                            resizeMode="cover"
-                            repeat={true}
-                            // playInBackground={true}
-                        />
-                        <Button onPress={revealTimer}>Start</Button>
-                    </View>
+                            <Video
+                                source={VIDEOS['9vnKR3QgneTKWChp4X1M']} // just update the source for example source={{ uri: 'http://www.example.com/video.mp4' }}
+                                style={{ width, height }}
+                                resizeMode='cover'
+                                repeat={true}
+                                // playInBackground={true}
+                            />
+                            <Button onPress={revealTimer}>Start</Button>
+                        </View>
                     )}
-                    
-                    
-                   
                 </VStack>
-                
-               
             </ScrollView>
         </NativeBaseProvider>
-    )
-}
+    );
+};
 
 export default RunExercise;
