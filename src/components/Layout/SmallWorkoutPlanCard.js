429aa5f7a3ba279d7dac09ac208a049a596042f8
Wonkeun No <59734889+Genne23v@users.noreply.github.com>
2023-03-26 18:14:24 -0400

    Issue 16 (#115)
    * Week10 video prep
    
    * Fix bugs for demo

--- src/components/Layout/SmallWorkoutPlanCard.js
+++ src/components/Layout/SmallWorkoutPlanCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, ScrollView } from "react-native";
-import { View, Image, HStack, Pressable, Text, Divider } from "native-base";
+import { Image, HStack, Pressable, Text } from 'native-base';
 import exerciseMedia from "../../exerciseContent/exerciseMedia";
 import { theme } from "../../theme/theme";
 
@@ -9,42 +9,63 @@ import PlaceHolderImage from "../../images/Biceps.png";
 
 const SmallWorkoutPlanCard = ({ title, imageSources, onPress }) => {
   return (
-    <Pressable style={styles.cardItem} onPress={onPress}>
-      <HStack style={styles.cardBody}>
-        <Text style={styles.title}>{title ? title : "WorkoutPlan Title"}</Text>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={true}
-          contentContainerStyle={{
-            width: 70 * (imageSources ? imageSources.length : 5) + 8,
-            borderRadius: 10,
-          }}
-        >
-          {imageSources ? (
-            imageSources.map((source, index) => (
-              <Image
-                key={index}
-                source={
-                  exerciseMedia[source].picture
-                    ? exerciseMedia[source].picture
-                    : PlaceHolderImage
-                }
-                style={styles.image}
-                alt=""
-              />
-            ))
-          ) : (
-            <>
-              <Image source={PlaceHolderImage} style={styles.image} alt="" />
-              <Image source={PlaceHolderImage} style={styles.image} alt="" />
-              <Image source={PlaceHolderImage} style={styles.image} alt="" />
-              <Image source={PlaceHolderImage} style={styles.image} alt="" />
-              <Image source={PlaceHolderImage} style={styles.image} alt="" />
-            </>
-          )}
-        </ScrollView>
-      </HStack>
-    </Pressable>
+      <Pressable style={styles.cardItem} onPress={onPress}>
+          <HStack style={styles.cardBody}>
+              <Text style={styles.title}>
+                  {title ? title : 'WorkoutPlan Title'}
+              </Text>
+              <ScrollView
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={true}
+                  contentContainerStyle={{
+                      width: 70 * (imageSources ? imageSources.length : 5) + 8,
+                      borderRadius: 10,
+                  }}>
+                  {imageSources ? (
+                      imageSources.map((source, index) => (
+                          <Image
+                              key={index}
+                              source={
+                                  exerciseMedia[source].picture
+                                      ? exerciseMedia[source].picture
+                                      : PlaceHolderImage
+                              }
+                              style={styles.image}
+                              alt='exercise image'
+                          />
+                      ))
+                  ) : (
+                      <>
+                          <Image
+                              source={PlaceHolderImage}
+                              style={styles.image}
+                              alt=''
+                          />
+                          <Image
+                              source={PlaceHolderImage}
+                              style={styles.image}
+                              alt=''
+                          />
+                          <Image
+                              source={PlaceHolderImage}
+                              style={styles.image}
+                              alt=''
+                          />
+                          <Image
+                              source={PlaceHolderImage}
+                              style={styles.image}
+                              alt=''
+                          />
+                          <Image
+                              source={PlaceHolderImage}
+                              style={styles.image}
+                              alt=''
+                          />
+                      </>
+                  )}
+              </ScrollView>
+          </HStack>
+      </Pressable>
   );
 };
 
