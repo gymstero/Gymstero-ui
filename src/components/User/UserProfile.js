fb6955c26c6baf09b6d3deb9697dfb16eb3c5288
Wonkeun No <zakk23v@hotmail.com>
2023-03-31 23:09:31 -0400

    Connect connection screen with backend

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -1,109 +1,120 @@
 import React, { useEffect, useState } from "react";
 import {
-  Container,
-  View,
-  Flex,
-  Image,
-  Spacer,
-  Text,
-  HStack,
-  NativeBaseProvider,
-  VStack,
-  ShareIcon,
-  ScrollView,
-} from "native-base";
-import UserInfo from "./UserInfo";
-import WorkoutInfo from "./WorkoutInfo";
-import { getUser, getIdToken } from "../auth/auth";
-import { customStyles } from "../../theme/customStyles";
+    Container,
+    Flex,
+    Image,
+    Spacer,
+    Text,
+    HStack,
+    NativeBaseProvider,
+    VStack,
+    ShareIcon,
+    ScrollView,
+} from 'native-base';
+import UserInfo from './UserInfo';
+import { getUser, getIdToken } from '../auth/auth';
+import { customStyles } from '../../theme/customStyles';
+
 const UserProfile = () => {
-  const [userData, setUserData] = useState({});
-  const [workoutData, setWorkoutData] = useState([]);
+    const [userData, setUserData] = useState({});
+    const [followingUsers, setFollowingUsers] = useState([]);
+    const [followers, setFollowers] = useState([]);
 
-  const fetchUserProfile = async () => {
-    const userInfo = await getUser();
-    const idToken = await getIdToken();
+    const fetchUserProfile = async () => {
+        const userInfo = await getUser();
+        const idToken = await getIdToken();
 
-    fetch(`http://10.0.2.2:8080/api/user/${userInfo.uid}/profile`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${idToken}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setUserData(res.userData);
-        console.log("User data fetched", userData);
-      })
-      .catch((err) => {
-        console.warn(err);
-      });
+        fetch(`http://10.0.2.2:8080/api/user/${userInfo.uid}/profile`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${idToken}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                setUserData(res.userData);
+                console.log('User data fetched', userData);
+            })
+            .catch((err) => {
+                console.warn(err);
+            });
 
-    fetch(`http://10.0.2.2:8080/api/user/${userInfo.uid}/workouts`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${idToken}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setWorkoutData(res.workouts);
-        console.log("Workout data fetched", workoutData);
-      })
-      .catch((err) => {
-        console.warn(err);
-      });
-  };
+        fetch(`http://10.0.2.2:8080/api/user/${userInfo.uid}/get-connections`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${idToken}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                setFollowingUsers(res.followingUsers);
+                setFollowers(res.followers);
+                console.log(
+                    'Connected user data fetched',
+                    followingUsers,
+                    followers
+                );
+            })
+            .catch((err) => {
+                console.warn(err);
+            });
+    };
 
-  useEffect(() => {
-    fetchUserProfile();
-  }, []);
-  return (
-    <NativeBaseProvider flex={1}>
-      <ScrollView>
-        <VStack style={customStyles.container}>
-          <Container mt={5}>
-            <Flex flexDirection="row">
-              <Image
-                source={{
-                  uri:
-                    userData.photoURL ||
-                    "https://img.icons8.com/ios-glyphs/90/000000/user--v1.png",
-                }}
-                alt="user profile"
-                size="xl"
-                flex={2}
-              />
-              <Container mx={5} flex={3} my="auto" py={2}>
-                <HStack>
-                  <Text fontSize={20} fontWeight={600}>
-                    {userData.username}
-                  </Text>
-                  <Spacer />
-                  <HStack>
-                    <ShareIcon size="lg" mr={4} />
-                    <Text fontSize={16}>{userData.numOfFollowers || "10"}</Text>
-                  </HStack>
-                </HStack>
+    useEffect(() => {
+        fetchUserProfile();
+    }, []);
+    return (
+        <NativeBaseProvider flex={1}>
+            <ScrollView>
+                <VStack style={customStyles.container}>
+                    <Container mt={8}>
+                        <Flex flexDirection='row'>
+                            <Image
+                                source={{
+                                    uri:
+                                        userData.photoURL ||
+                                        'https://img.icons8.com/ios-glyphs/90/000000/user--v1.png',
+                                }}
+                                alt='user profile'
+                                size='xl'
+                                flex={2}
+                            />
+                            <Container mx={5} flex={3} my='auto' py={2}>
+                                <HStack>
+                                    <Text fontSize={20} fontWeight={600}>
+                                        {userData.username}
+                                    </Text>
+                                    <Spacer />
+                                    <HStack>
+                                        <ShareIcon size='lg' mr={4} />
+                                        <Text fontSize={16}>
+                                            {userData.numOfFollowers || '10'}
+                                        </Text>
+                                    </HStack>
+                                </HStack>
 
-                <Text py={2}>{userData.description}</Text>
-              </Container>
-            </Flex>
-          </Container>
-          <Container my={5} width="100%">
-            <WorkoutInfo workouts={workoutData} />
-          </Container>
-          <Container my={5}>
-            <UserInfo />
-          </Container>
-        </VStack>
-      </ScrollView>
-    </NativeBaseProvider>
-  );
+                                <Text py={2}>{userData.description}</Text>
+                            </Container>
+                        </Flex>
+                    </Container>
+
+                    <Container my={5}>
+                        <UserInfo
+                            title='Following Users'
+                            users={followingUsers}
+                        />
+                    </Container>
+                    <Container my={5}>
+                        <UserInfo title='Followers' users={followers} />
+                    </Container>
+                </VStack>
+            </ScrollView>
+        </NativeBaseProvider>
+    );
 };
 
 export default UserProfile;
--- src/theme/customStyles.js
+++ src/theme/customStyles.js
@@ -1,41 +1,41 @@
 import { Center } from "native-base";
 import { theme } from "./theme";
 export const customStyles = {
-  container: {
-    padding: 20,
-    alignItems: "center",
-    flex: 1,
-    backgroundColor: theme.colors.background,
-  },
-  header: {
-    // backgroundColor: theme.colors.primary,
-    width: "100%",
-    fontFamily: "Arial",
-    fontSize: 22,
-    fontWeight: "bold",
-    // color: theme.colors.text,
-    textAlign: "center",
-    padding: 10,
-  },
-  body: {
-    //fontFamily: "Arial",
-    fontSize: 15,
-    border: 1,
-    padding: 5,
-  },
-  h1: {
-    fontFamily: "Arial",
-    fontSize: 32,
-    fontWeight: "bold",
-    padding: 10,
-  },
-  loadingContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  loading: {
-    //size: "large",
-    color: theme.colors.secondary,
-  },
+    container: {
+        padding: 2,
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: theme.colors.background,
+    },
+    header: {
+        // backgroundColor: theme.colors.primary,
+        width: '100%',
+        fontFamily: 'Arial',
+        fontSize: 22,
+        fontWeight: 'bold',
+        // color: theme.colors.text,
+        textAlign: 'center',
+        padding: 10,
+    },
+    body: {
+        //fontFamily: "Arial",
+        fontSize: 15,
+        border: 1,
+        padding: 5,
+    },
+    h1: {
+        fontFamily: 'Arial',
+        fontSize: 32,
+        fontWeight: 'bold',
+        padding: 10,
+    },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loading: {
+        //size: "large",
+        color: theme.colors.secondary,
+    },
 };
