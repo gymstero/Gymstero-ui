2d435d77d61c8e1dae6c0dfe9242ba5dd01b55a4
Wonkeun No <59734889+Genne23v@users.noreply.github.com>
2023-03-31 23:12:29 -0400

    Connect connection screen with backend (#126)

--- src/components/User/UserInfo.js
+++ src/components/User/UserInfo.js
@@ -1,72 +1,47 @@
 import React from "react";
-import {
-  Container,
-  Flex,
-  Text,
-  HStack,
-  VStack,
-  ShareIcon,
-  View,
-} from "native-base";
-import { customStyles } from "../../theme/customStyles";
-const UserInfo = () => {
-  return (
-    <VStack space={4} w="100%">
-      <Text fontSize={20} fontWeight={600}>
-        Following Users
-      </Text>
-      <HStack
-        style={{
-          borderColor: "gray.100",
-          borderStyle: "solid",
-          borderWidth: 0.5,
-        }}
-      >
-        <Container w={5} bg="gray.300" />
-        <VStack w="90%" ml={2} py={2} px={3} space={2}>
-          <Flex
-            flexDirection="row"
-            justifyContent="space-between"
-            borderBottomWidth={0.5}
-          >
-            <Text fontSize={18} fontWeight={600}>
-              Blake
+import { Container, Flex, Text, HStack, VStack, ShareIcon } from 'native-base';
+import { theme } from '../../theme/theme';
+
+const UserInfo = (props) => {
+    const { title, users } = props;
+
+    return (
+        <VStack space={4} w='100%'>
+            <Text fontSize={20} fontWeight={600}>
+                {title}
             </Text>
-            <HStack>
-              <ShareIcon size="lg" mr={4} />
-              <Text fontSize={16}>{"23"}</Text>
-            </HStack>
-          </Flex>
-          <Text>What to add here?</Text>
+            {users.map((user) => {
+                return (
+                    <HStack
+                        key={user.id}
+                        style={{
+                            borderColor: 'gray.100',
+                            borderStyle: 'solid',
+                            borderWidth: 0.5,
+                        }}>
+                        <Container w={5} bg={theme.colors.primary} />
+                        <VStack w='90%' ml={2} py={2} px={3} space={2}>
+                            <Flex
+                                flexDirection='row'
+                                justifyContent='space-between'
+                                borderBottomWidth={0.5}>
+                                <Text fontSize={18} fontWeight={600}>
+                                    {user.username}
+                                </Text>
+                                <HStack>
+                                    <ShareIcon size='lg' mr={4} />
+                                    <Text fontSize={16}>
+                                        {user.numOfFollowers}
+                                    </Text>
+                                </HStack>
+                            </Flex>
+                            <Text>{user.description}</Text>
+                        </VStack>
+                    </HStack>
+                );
+            })}
         </VStack>
-      </HStack>
-      <HStack
-        style={{
-          borderColor: "gray.100",
-          borderStyle: "solid",
-          borderWidth: 0.5,
-        }}
-      >
-        <Container w={5} bg="gray.300" />
-        <VStack w="90%" ml={2} py={2} px={3} space={2}>
-          <Flex
-            flexDirection="row"
-            justifyContent="space-between"
-            borderBottomWidth={0.5}
-          >
-            <Text fontSize={18} fontWeight={600}>
-              Rachel
-            </Text>
-            <HStack>
-              <ShareIcon size="lg" mr={4} />
-              <Text fontSize={16}>{"10"}</Text>
-            </HStack>
-          </Flex>
-          <Text>What to add here?</Text>
-        </VStack>
-      </HStack>
-    </VStack>
-  );
+    );
 };
 
 export default UserInfo;
